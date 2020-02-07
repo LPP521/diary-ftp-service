e206ea0505eba7bfbfa72dc3839c4c4cb6852c61
gzgy-leoyang <leoyang20102013@163.com>
2020-02-07 21:22:23 +0800

    添加客户端列表，标记可以连接的客户名称，密码，目录等

--- diary-ftp-service.py
+++ diary-ftp-service.py
@@ -3,24 +3,47 @@ from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 import logging
+import sys
+import re
+
+def get_client_list( list_file_path):
+    user_list = []
+    line_index = 0
+    with open ( list_file_path , "r") as fd:
+        for line in fd:
+            line_index = line_index + 1
+            if not line.__contains__("#"):
+                line = line.strip("\n")
+                list = line.split(",")
+                if len( list ) == 4 :
+                    print ("str:", list[2] )
+                    if not re.match(  r'^[elradfmwMT]{1,10}$' ,list[2]):
+                        print ( "用户权限错误 line_%d,%s" % (line_index,list_file_path))
+                        list[2] = "elradfmwM"
+                    user_list.append(  list )
+                else :
+                    print ( "格式错误 line_%d,%s" % (line_index,list_file_path))
+    return user_list
+
 
 def main():
     # 新建一个“授权者”，处理 ftp 登陆过程的鉴权，这个对象被 FTPHandler 使用，
     # 用于鉴别登陆用户的密码，返回登陆用户的目录，检查读写权限等
     authorizer = DummyAuthorizer()
-    authorizer.add_user("user","123","./ftp-user",perm="elradfmwM")
+    user_list = get_client_list( sys.path[0] + "/client_list" )
+    for user in user_list:
+        user_name,password,permit,homedir = user
+        if not os.access( homedir , os.F_OK ):
+            os.mkdir( homedir )
+        authorizer.add_user( user_name,password,homedir,perm=permit ,msg_login="Welcome"+user_name ,msg_quit="Bye"+user_name)    
     authorizer.add_anonymous( "./ftp-anonymous",perm="elradfmwM")
-    # 可以如下设置：
-    # authorizer.add_user(username,password,homedir,perm="elr",mas_login="Login successful",msg_quit="Goodbye")
-    
+
     # FTPHandler,实现 FTP 协议内容，处理来自客户端的命令，所有会话的信息存储在这个实例变量中。 
     handler = FTPHandler
     handler.authorizer = authorizer
 
-    # logging.basicConfig(level=logging.DEBUG)
-
     # 连接成功是发送的字符串
-    handler.banner = "ftpd OK"
+    handler.banner = "Welcome to FTP Server"
 
     address = ('',21)
     server = FTPServer(address,handler)
@@ -33,7 +56,6 @@ def main():
     # 启动异步IO循环
     #server.serve_forever( timeout=None,blocking=True,handle_exit=True)
     server.serve_forever()
-
 
 if __name__ == "__main__":
     main()
